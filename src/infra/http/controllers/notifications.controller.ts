e3116fd4b04cce0888d67be511117676b6c86234
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-02-03 18:19:01 -0300

    Add viewModel

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { CreateNotificationBody } from '../dtos/create-notification-body'
 import { SendNotificationUseCase } from '@/app/use-cases/send-notification-use-case'
+import { NotificationViewModel } from '../view-models/notification-view-model'
 
 @Controller('notifications')
 export class NotificationsController {
@@ -15,6 +16,8 @@ export class NotificationsController {
       category,
     })
 
-    return { notification }
+    const viewModel = NotificationViewModel.toHTTP(notification)
+
+    return { notification: viewModel }
   }
 }
